4acd7d31128cd85642f05196e3afb5048427e6e9
koltona <droyktton@gmail.com>
2021-06-01 20:21:09 -0300

    update

--- medidoresIB/leerserial.py
+++ medidoresIB/leerserial.py
@@ -21,6 +21,7 @@ H=[]
 
 start=time.time()
 with open('medidor.dat', 'w') as the_file:
+    the_file.write("fecha,elap,co2,temp,humedad\n")
     with serial.Serial('/dev/ttyACM0', 9600, timeout=1) as ser:
         for i in range(0,N,1):
             line = ser.readline()
@@ -29,17 +30,15 @@ with open('medidor.dat', 'w') as the_file:
                 lista=s.split(sep=" ")
                 if lista[0]=="co2(ppm):":
                     elap=time.time()-start
-                    the_file.write(str(datetime.datetime.now())+" "+str(elap)+" "+str(lista[1])+" "+str(lista[3])+" "+str(lista[5]))
+                    the_file.write(str(datetime.datetime.now())+","+str(elap)+","+str(lista[1])+","+str(lista[3])+","+str(lista[5]))
                     co2.append(lista[1])
                     T.append(lista[3])
                     H.append(lista[5])
                 the_file.flush()
 
-# serie temporal de co2
-# plt.plot(np.arange(0,len(co2)), co2, label='CO2')
-# plt.legend()
-# plt.show()
-
+# por ejemplo, uno podria el script en backgrou y correr cada 5 minutos, estos comandos, para visualizar en una pagina web
+# cp medidor.dat ~/Codigos/loscoihues/ventilacion/
+# git add medidor.dat dibujo.html; git commit -m "medidor CO2 update"; git push
 
 
 
