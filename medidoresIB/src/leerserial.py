343a12bacf51c156f371f827c2a2e82187a5f6dc
koltona <droyktton@gmail.com>
2021-06-14 14:44:41 -0300

    leer serial prints

--- medidoresIB/src/leerserial.py
+++ medidoresIB/src/leerserial.py
@@ -6,23 +6,23 @@
 
 #instalar serial:
 #https://pyserial.readthedocs.io/en/latest/pyserial.html#installation
-
 import serial
+
 import numpy as np
 import matplotlib.pyplot as plt
 import time
 import datetime
 
+#un numero máximo de lecturas
 N=10000000
-
-co2=[]
-T=[]
-H=[]
 
 start=time.time()
 with open('medidor.dat', 'w') as the_file:
+    print("preparado el archivo (medidor.dat)")
     the_file.write("fecha,elap,co2,temp,humedad\n")
     with serial.Serial('/dev/ttyACM0', 9600, timeout=1) as ser:
+        print("preparado el puerto serial")
+        print("midiendo...")
         for i in range(0,N,1):
             line = ser.readline()
             s=line.decode("utf-8")
@@ -31,14 +31,7 @@ with open('medidor.dat', 'w') as the_file:
                 if lista[0]=="co2(ppm):":
                     elap=time.time()-start
                     the_file.write(str(datetime.datetime.now())+","+str(elap)+","+str(lista[1])+","+str(lista[3])+","+str(lista[5]))
-                    co2.append(lista[1])
-                    T.append(lista[3])
-                    H.append(lista[5])
                 the_file.flush()
-
-# por ejemplo, uno podria el script en backgrou y correr cada 5 minutos, estos comandos, para visualizar en una pagina web
-# cp medidor.dat ~/Codigos/loscoihues/ventilacion/
-# git add medidor.dat dibujo.html; git commit -m "medidor CO2 update"; git push
 
 
 
